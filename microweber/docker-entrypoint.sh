7a7d49227331952a0b6775d43cb6c3bfb9e94006
karser <dpoddubny@gmail.com>
2019-03-25 02:39:05 +0200

    Initial release

---
+++ microweber/docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+# wait for db
+WAIT=0; echo "Waiting for ${DB_HOST} to start"
+while ! nc -z ${DB_HOST} ${DB_PORT}; do [ "$WAIT" -gt 60 ] && echo "Error: DB timeout" && exit 1 || sleep 1 && WAIT=$(($WAIT + 1)); done
+echo "DB is up"
+
+if [[ "$1" == httpd* ]]; then
+
+        # Enable commonly used apache modules
+        sed -i '/LoadModule rewrite_module/s/^#//g' /etc/apache2/httpd.conf
+        sed -i '/LoadModule deflate_module/s/^#//g' /etc/apache2/httpd.conf
+        sed -i '/LoadModule expires_module/s/^#//g' /etc/apache2/httpd.conf
+
+        sed -i "s#^DocumentRoot \".*#DocumentRoot \"/var/www/microweber\"#g" /etc/apache2/httpd.conf
+        sed -i "s#/var/www/localhost/htdocs#/var/www/microweber#" /etc/apache2/httpd.conf
+        printf "\n<Directory \"/var/www/microweber\">\n\tOptions Indexes FollowSymLinks\n\tAllowOverride All\n\tRequire all granted\n</Directory>\n" >> /etc/apache2/httpd.conf
+
+
+        if [ `ls -A /var/www/microweber/config | wc -m` == "0" ]; then
+            cp -r /var/www/microweber-defaults/config /var/www/microweber
+            chown -R apache:apache /var/www/microweber/config
+        fi
+
+        if [ `ls -A /var/www/microweber/userfiles | wc -m` == "0" ]; then
+            cp -r /var/www/microweber-defaults/userfiles /var/www/microweber
+            chown -R apache:apache /var/www/microweber/userfiles
+        fi
+
+        if [ -f "/var/www/microweber/config/microweber.php" ]; then
+                echo "CMS is installed, skipping"
+        else
+                sudo --preserve-env -u apache php /var/www/microweber/artisan microweber:install \
+                    ${MW_EMAIL} ${MW_USER} ${MW_PASSWORD} \
+                    ${DB_HOST} ${DB_NAME} ${DB_USER} ${DB_PASS} ${DB_DRIVER} ${DB_PREFIX}
+
+                chown -R apache:apache /var/www/microweber/storage
+        fi
+fi
+
+
+exec "$@"
---
+++ microweber/readme.md
@@ -0,0 +1,61 @@
+Dockerized Microweber CMS
+=========================
+
+
+The difference from the [official](https://github.com/microweber/docker) docker image:
+- Apache is running on Alpine. In the original image Debian is used.
+- Solved potential conflict of config/userfiles folders.
+They are tracked in the repo and at the same time are volumes
+that can be modified by user.
+- The docker image contains up-to-date CMS version.
+
+
+Docker compose example:
+```
+version: '3.3'
+services:
+    microweber:
+        image: karser/microweber:1.1.3
+        volumes:
+            - ./config:/var/www/microweber/config
+            - ./userfiles:/var/www/microweber/userfiles
+        environment:
+            - DB_USER=microweber
+            - DB_PASS=microweber
+            - DB_NAME=microweber
+            - DB_ENGINE=pgsql
+            - DB_HOST=db
+            - DB_PORT=5432
+            - DB_PREFIX=
+            - MW_EMAIL=your@email.com
+            - MW_USER=admin
+            - MW_PASSWORD=******
+            - MW_TEMPLATE=liteness
+        depends_on:
+            - db
+        restart: on-failure:10
+        ports:
+            - 8082:80
+    db:
+        image: postgres:9.6-alpine
+        volumes:
+            - ./db_data:/var/lib/postgresql/data
+        environment:
+            - POSTGRES_USER=microweber
+            - POSTGRES_PASSWORD=microweber
+            - POSTGRES_DB=microweber
+        restart: on-failure:10
+```
+
+
+Manual build
+```
+export MICROWEBER_VERSION=1.1.2
+ 
+docker build --build-arg MICROWEBER_VERSION=${MICROWEBER_VERSION}.x-dev \
+    --tag karser/microweber:$MICROWEBER_VERSION \
+    --tag karser/microweber:latest .
+
+docker push karser/microweber:$MICROWEBER_VERSION
+docker push karser/microweber:latest
+```
